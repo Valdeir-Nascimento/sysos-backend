316e123f148ebd0dc4c0a91d1a98326a54867335
Valdeir Nascimento <valdeircn11@gmail.com>
2022-10-27 13:42:25 -0300

    Criação do projeto

---
+++ src/main/java/br/com/sysos/SysosBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.sysos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sysos/SysosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sysos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
